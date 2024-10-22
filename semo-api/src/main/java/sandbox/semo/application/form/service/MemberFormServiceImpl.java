a0a7562024c9a8124619d54002c0014b4a2c5acc
dabeen-jung <ltea99@naver.com>
2024-10-23 00:07:06 +0900

    :sparkles: feat : MemberForm 요청처리 API log 추가

--- semo-api/src/main/java/sandbox/semo/application/form/service/MemberFormServiceImpl.java
+++ semo-api/src/main/java/sandbox/semo/application/form/service/MemberFormServiceImpl.java
@@ -3,6 +3,7 @@ package sandbox.semo.application.form.service;
 import static sandbox.semo.application.form.exception.MemberFormErrorCode.COMPANY_NOT_EXIST;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sandbox.semo.application.form.exception.MemberFormBusinessException;
@@ -14,27 +15,27 @@ import sandbox.semo.domain.form.entity.Status;
 import sandbox.semo.domain.form.repository.MemberFormRepository;
 
 @Service
+@Log4j2
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberFormServiceImpl implements MemberFormService {
 
     private final MemberFormRepository memberFormRepository;
     private final CompanyRepository companyRepository;
-    
+
     @Override
     @Transactional
     public void formRegister(MemberFormRegister request) {
         Company requestCompany = companyRepository.findById(request.getCompanyId())
                 .orElseThrow(() -> new MemberFormBusinessException(COMPANY_NOT_EXIST));
 
-        MemberForm memberForm = MemberForm.builder()
+        memberFormRepository.save(MemberForm.builder()
                 .companyName(requestCompany.getCompanyName())
                 .email(request.getEmail())
                 .ownerName(request.getOwnerName())
                 .status(Status.PENDING)
-                .build();
-
-        memberFormRepository.save(memberForm);
+                .build());
+        log.info(">>> [ ✅ 고객사 회원가입 폼이 성공적으로 등록되었습니다. ]");
     }
 
 }
