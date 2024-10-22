fae7dc0a1f855e3892b63c81bd6226a7b8a95b6d
dabeen-jung <ltea99@naver.com>
2024-10-23 00:22:13 +0900

    :hammer: fix : MemberForm DTO 유효성 검증 수정

--- semo-core/src/main/java/sandbox/semo/domain/form/dto/request/MemberFormRegister.java
+++ semo-core/src/main/java/sandbox/semo/domain/form/dto/request/MemberFormRegister.java
@@ -2,12 +2,13 @@ package sandbox.semo.domain.form.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class MemberFormRegister {
 
-    @NotBlank
+    @NotNull
     private Long companyId;
 
     @Email
