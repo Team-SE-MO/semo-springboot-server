43c6420e090141cf3367f1978e96b7dd732a7811
dabeen-jung <ltea99@naver.com>
2024-10-23 19:51:33 +0900

    :recycle: refactor : 공통 form 구성을 위한 common 패키지로 이동

--- semo-core/src/main/java/sandbox/semo/domain/common/entity/BaseForm.java
+++ semo-core/src/main/java/sandbox/semo/domain/common/entity/BaseForm.java
@@ -1,4 +1,4 @@
-package sandbox.semo.domain.form.entity;
+package sandbox.semo.domain.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
--- semo-core/src/main/java/sandbox/semo/domain/common/entity/Status.java
+++ semo-core/src/main/java/sandbox/semo/domain/common/entity/Status.java
@@ -1,4 +1,4 @@
-package sandbox.semo.domain.form.entity;
+package sandbox.semo.domain.common.entity;
 
 public enum Status {
     PENDING, DENIED, APPROVED;
