9ce046f3998eb43e042c26ce797b52908e603349
dabeen-jung <ltea99@naver.com>
2024-10-24 00:18:52 +0900

    :sparkles: feat : 고객사 회원가입 폼 요청API 예외처리 추가

--- semo-api/src/main/java/sandbox/semo/application/member/exception/MemberErrorCode.java
+++ semo-api/src/main/java/sandbox/semo/application/member/exception/MemberErrorCode.java
@@ -17,7 +17,8 @@ public enum MemberErrorCode implements ErrorCode {
     UNAUTHORIZED_TO_MEMBER(FORBIDDEN, "권한이 없는 사용자입니다."),
     WRONG_PASSWORD(BAD_REQUEST, "입력하신 비밀번호를 다시 확인해 주세요."),
     COMPANY_NOT_EXIST(NOT_FOUND, "존재하지 않는 회사입니다."),
-    FORM_DOES_NOT_EXIST(NOT_FOUND, "해당 ID의 폼을 찾을 수 없습니다.");
+    FORM_DOES_NOT_EXIST(NOT_FOUND, "해당 ID의 폼을 찾을 수 없습니다."),
+    INVALID_COMPANY_SELECTION(BAD_REQUEST, "선택할 수 없는 회사입니다.");
 
     private final HttpStatus httpStatus;
 
--- semo-api/src/main/java/sandbox/semo/application/member/service/MemberServiceImpl.java
+++ semo-api/src/main/java/sandbox/semo/application/member/service/MemberServiceImpl.java
@@ -37,4 +37,62 @@ public class MemberServiceImpl implements MemberService {
                 .orElseThrow(() -> new RuntimeException("Company not found"));
     }
 
+
+    @Transactional
+    @Override
+    public void formRegister(MemberFormRegister request) {
+        if (request.getCompanyId() == 1L) {
+            throw new MemberBusinessException(INVALID_COMPANY_SELECTION);
+        }
+
+        Company requestCompany = companyRepository.findById(request.getCompanyId())
+                .orElseThrow(() -> new MemberBusinessException(COMPANY_NOT_EXIST));
+
+        memberFormRepository.save(MemberForm.builder()
+                .companyName(requestCompany.getCompanyName())
+                .email(request.getEmail())
+                .ownerName(request.getOwnerName())
+                .formStatus(FormStatus.PENDING)
+                .build());
+        log.info(">>> [ ✅ 고객사 회원가입 폼이 성공적으로 등록되었습니다. ]");
+    }
+
+    /**
+     * TODO: 0번째 에지부터 data가 없으면, 빈배열
+     * totalPage를 넘어갔을 때 data가 없으면 예외처리 발생
+     **/
+    @Override
+    public Page<MemberFormInfo> findAllForms(int page, int size) {
+        List<Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("requestDate"));
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sorts));
+        Page<MemberForm> memberFormPage = memberFormRepository.findAll(pageable);
+
+        return memberFormPage.map(memberForm -> MemberFormInfo.builder()
+                .formId(memberForm.getId())
+                .companyName(memberForm.getCompanyName())
+                .ownerName(memberForm.getOwnerName())
+                .email(memberForm.getEmail())
+                .formStatus(memberForm.getFormStatus())
+                .requestDate(memberForm.getRequestDate())
+                .approvedAt(memberForm.getApprovedAt())
+                .build());
+    }
+
+
+    @Override
+    @Transactional
+    public String updateForm(MemberFormDecision request) {
+        MemberForm memberForm = memberFormRepository.findById(request.getFormId())
+                .orElseThrow(() -> new MemberBusinessException(FORM_DOES_NOT_EXIST));
+
+        FormStatus newFormStatus = FormStatus.valueOf(request.getDecisionStatus().toUpperCase());
+        memberForm.changeStatus(newFormStatus);
+        MemberForm saveForm = memberFormRepository.save(memberForm);
+        log.info(">>> [ ✅ 고객사 회원가입 폼을 관리자가 최종 처리하였습니다. ]");
+        return saveForm.getFormStatus().toString();
+    }
+
+
 }
