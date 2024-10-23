d14e40c00dae860f73f9801b44c86b518cd1771f
dabeen-jung <ltea99@naver.com>
2024-10-23 22:10:53 +0900

    :recycle: refactor : CompanyForm 패키지의 Controller,ErrorCode ->  Company 패키지로 코드 이동 및 병합

--- semo-api/src/main/java/sandbox/semo/application/company/controller/CompanyController.java
+++ semo-api/src/main/java/sandbox/semo/application/company/controller/CompanyController.java
@@ -2,18 +2,26 @@ package sandbox.semo.application.company.controller;
 
 import static org.springframework.http.HttpStatus.OK;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sandbox.semo.application.common.response.ApiResponse;
+import sandbox.semo.application.company.service.CompanyFormService;
 import sandbox.semo.application.company.service.CompanyService;
+import sandbox.semo.domain.company.dto.request.CompanyFormDecision;
+import sandbox.semo.domain.company.dto.request.CompanyFormRegister;
+import sandbox.semo.domain.company.dto.response.CompanyFormInfo;
 import sandbox.semo.domain.company.entity.Company;
 
 
@@ -24,6 +32,7 @@ import sandbox.semo.domain.company.entity.Company;
 public class CompanyController {
 
     private final CompanyService companyService;
+    private final CompanyFormService companyFormService;
 
     @PreAuthorize("hasRole('SUPER')")
     @PostMapping("/{id}")
@@ -42,6 +51,43 @@ public class CompanyController {
         return ApiResponse.successResponse(
                 OK,
                 "성공적으로 회사 목록을 조회하였습니다.",
+                data
+        );
+    }
+
+    @PostMapping("/form")
+    public ApiResponse<Void> formRegister(
+            @RequestBody @Valid CompanyFormRegister registerForm) {
+        companyFormService.formRegister(registerForm);
+        return ApiResponse.successResponse(
+                OK,
+                "성공적으로 폼을 제출하였습니다.");
+    }
+
+    @PreAuthorize("hasRole('SUPER')")
+    @GetMapping("/form")
+    public ApiResponse<Page<CompanyFormInfo>> registerList(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Page<CompanyFormInfo> companyFormLists = companyFormService.findAllForms(page, size);
+        return ApiResponse.successResponse(
+                OK,
+                "성공적으로 목록을 조회하였습니다.",
+                companyFormLists
+        );
+
+    }
+
+    @PreAuthorize("hasRole('SUPER')")
+    @PatchMapping("/form")
+    public ApiResponse<String> formUpdate(
+            @RequestBody @Valid CompanyFormDecision updateForm) {
+        String data = companyFormService.updateStatus(updateForm);
+
+        return ApiResponse.successResponse(
+                OK,
+                "성공적으로 처리되었습니다.",
                 data
         );
     }
