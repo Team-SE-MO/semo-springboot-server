4fbfa29251052691eb49b4f0c44c4491aff56334
dabeen-jung <ltea99@naver.com>
2024-10-23 10:04:00 +0900

    :sparkles: feat : MemberForm 목록조회 API 응답 DTO 구현

---
+++ semo-core/src/main/java/sandbox/semo/domain/form/dto/response/MemberFormList.java
@@ -0,0 +1,37 @@
+package sandbox.semo.domain.form.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sandbox.semo.domain.form.entity.Status;
+
+@Getter
+@NoArgsConstructor
+public class MemberFormList {
+
+    private Long formId;
+    private String companyName;
+    private String ownerName;
+    private String email;
+    private Status status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime requestDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime approvedAt;
+
+    @Builder
+    public MemberFormList(Long formId, String companyName, String ownerName,
+            String email, Status status, LocalDateTime requestDate, LocalDateTime approvedAt) {
+        this.formId = formId;
+        this.companyName = companyName;
+        this.ownerName = ownerName;
+        this.email = email;
+        this.status = status;
+        this.requestDate = requestDate;
+        this.approvedAt = approvedAt;
+    }
+}
