c071709c1729a0bdc88d41bc8fe693e07523c58c
dabeen-jung <ltea99@naver.com>
2024-10-23 11:36:50 +0900

    :sparkles: feat : MemberForm 관리자 승인처리 API 비즈니스 로직 구현

--- semo-api/src/main/java/sandbox/semo/application/form/service/MemberFormService.java
+++ semo-api/src/main/java/sandbox/semo/application/form/service/MemberFormService.java
@@ -1,6 +1,7 @@
 package sandbox.semo.application.form.service;
 
 import org.springframework.data.domain.Page;
+import sandbox.semo.domain.form.dto.request.MemberFormDecision;
 import sandbox.semo.domain.form.dto.request.MemberFormRegister;
 import sandbox.semo.domain.form.dto.response.MemberFormList;
 
@@ -9,4 +10,6 @@ public interface MemberFormService {
     void formRegister(MemberFormRegister request);
 
     Page<MemberFormList> findAllForms(int page, int size);
+
+    String updateForm(MemberFormDecision request);
 }
