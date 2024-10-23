f428f514d98e41c12caf1d867ea28cb4eb3ea6e2
dabeen-jung <ltea99@naver.com>
2024-10-24 00:19:18 +0900

    :recycle: refactor : MemberFormService ->  MemberService 코드 병합

--- semo-api/src/main/java/sandbox/semo/application/member/service/MemberServiceImpl.java
+++ semo-api/src/main/java/sandbox/semo/application/member/service/MemberServiceImpl.java
@@ -1,26 +1,49 @@
 package sandbox.semo.application.member.service;
 
+import static sandbox.semo.application.member.exception.MemberErrorCode.COMPANY_NOT_EXIST;
+import static sandbox.semo.application.member.exception.MemberErrorCode.FORM_DOES_NOT_EXIST;
+import static sandbox.semo.application.member.exception.MemberErrorCode.INVALID_COMPANY_SELECTION;
+
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import sandbox.semo.domain.member.dto.request.MemberRegister;
+import org.springframework.transaction.annotation.Transactional;
+import sandbox.semo.application.member.exception.MemberBusinessException;
+import sandbox.semo.domain.common.entity.FormStatus;
 import sandbox.semo.domain.company.entity.Company;
 import sandbox.semo.domain.company.repository.CompanyRepository;
+import sandbox.semo.domain.member.dto.request.MemberFormDecision;
+import sandbox.semo.domain.member.dto.request.MemberFormRegister;
+import sandbox.semo.domain.member.dto.request.MemberRegister;
+import sandbox.semo.domain.member.dto.response.MemberFormInfo;
 import sandbox.semo.domain.member.entity.Member;
+import sandbox.semo.domain.member.entity.MemberForm;
 import sandbox.semo.domain.member.entity.Role;
+import sandbox.semo.domain.member.repository.MemberFormRepository;
 import sandbox.semo.domain.member.repository.MemberRepository;
 
 // TODO: Security 설정 확인 용도로 가볍게 구현 했음. Member API 개발시 추가 코드 필요.
 @Service
+@Log4j2
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final MemberFormRepository memberFormRepository;
     private final CompanyRepository companyRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
+    @Transactional
     public void register(MemberRegister request) {
         Member member = Member.builder()
                 .company(getCompanyById(request.getCompanyId()))
