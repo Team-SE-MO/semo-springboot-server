b8288c228f3feaa6978fcd75d1484642fcf1d8db
dabeen-jung <ltea99@naver.com>
2024-10-26 20:13:46 +0900

    :recycle: refactor : 로그인 id 생성 - 회사별 99개 생성으로 수정 및 동시성 문제 적용

--- semo-api/src/main/java/sandbox/semo/application/common/util/LoginIdGeneratorUtil.java
+++ semo-api/src/main/java/sandbox/semo/application/common/util/LoginIdGeneratorUtil.java
@@ -1,21 +1,23 @@
 package sandbox.semo.application.common.util;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class LoginIdGeneratorUtil {
 
-    private static int lastNumber = 1;
+    private static final Map<String, AtomicInteger> lastNumberMap = new ConcurrentHashMap<>();
 
     public static String generateLoginId(String role, String taxId) {
-        String prefix = "";
-        if (role.equals("ADMIN")) {
-            prefix = "A";
-        } else {
-            prefix = "U";
-        }
+        String prefix = role.equals("ADMIN") ? "A" : "U";
 
         String splitTaxId = taxId.replace("-", "");
-        String lastTwoDigits = String.format("%02d", lastNumber);
 
-        lastNumber += 1;
+        //taxId가 이미 존재하면 pass, 존재하지 않다면 taxId로 키와 초기값 생성해 Map에 넣어줌
+        AtomicInteger lastNumber = lastNumberMap.computeIfAbsent(splitTaxId,
+                k -> new AtomicInteger(1));
+
+        String lastTwoDigits = String.format("%02d", lastNumber.getAndIncrement());
 
         return prefix + splitTaxId + lastTwoDigits;
     }
