753463a6835a4edebe387baa40d47d244d5501ed
heonuk <gitshineit@gmail.com>
2024-10-26 22:28:03 +0900

    :sparkles: feat: Device 상태 Step Processor 클래스 생성

---
+++ semo-batch/src/main/java/sandbox/semo/batch/service/step/DeviceStatusProcessor.java
@@ -0,0 +1,58 @@
+package sandbox.semo.batch.service.step;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.item.ItemProcessor;
+import sandbox.semo.batch.dto.DeviceStatus;
+import sandbox.semo.batch.util.HikariDataSourceUtil;
+import sandbox.semo.domain.common.crypto.AES256;
+import sandbox.semo.domain.device.entity.Device;
+
+@Log4j2
+@RequiredArgsConstructor
+public class DeviceStatusProcessor implements ItemProcessor<Device, DeviceStatus>,
+        StepExecutionListener {
+
+    private final AES256 aes256;
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+        log.info(">>> [ 🚀 Device Processor 초기화 ]");
+    }
+
+    @Override
+    public DeviceStatus process(Device device) {
+        HikariDataSource dataSource = null;
+        boolean updatedStatus;
+
+        try {
+            dataSource = HikariDataSourceUtil.createDataSource(device, aes256);
+            dataSource.getConnection().isValid(1);
+            updatedStatus = true;
+            log.info(">>> ✅ Device {} 연결 성공. 상태: true", device.getDeviceAlias());
+        } catch (Exception e) {
+            updatedStatus = false;
+            log.error(">>> ❌ Device {} 연결 실패. 상태: 오류. 에러: {}",
+                    device.getDeviceAlias(),
+                    e.getMessage());
+        } finally {
+            if (dataSource != null) {
+                dataSource.close();
+            }
+        }
+
+        boolean statusChanged = device.getStatus() != updatedStatus;
+        return new DeviceStatus(device, statusChanged);
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        log.info(">>> [ ✅ 모든 장비 작업 완료 ]");
+        return ExitStatus.COMPLETED;
+    }
+
+}
