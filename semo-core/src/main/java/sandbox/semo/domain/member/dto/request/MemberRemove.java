1c891fc0ade84c699ada5de9296dcbe6632d9170
dabeen-jung <ltea99@naver.com>
2024-10-29 17:12:08 +0900

    :recycle: refactor : 회원삭제 api - 비즈니스 로직 단 DTO 추가

---
+++ semo-core/src/main/java/sandbox/semo/domain/member/dto/request/MemberRemove.java
@@ -0,0 +1,31 @@
+package sandbox.semo.domain.member.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import sandbox.semo.domain.member.entity.Role;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class MemberRemove {
+
+    @NotNull
+    private Role role;
+
+    @NotNull
+    private Long companyId;
+
+    @NotNull
+    private String loginId;
+
+    public static MemberRemove of(String loginId, Long companyId, Role role) {
+        return MemberRemove.builder()
+                .companyId(companyId)
+                .loginId(loginId)
+                .role(role)
+                .build();
+    }
+
+}
