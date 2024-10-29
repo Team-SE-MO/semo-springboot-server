412e832e55dca6f09770626eb911f9e917de5ffc
dabeen-jung <ltea99@naver.com>
2024-10-29 13:58:41 +0900

    :hammer: fix : @PreAuthorize 적용 안되는 부분 해결

--- semo-api/src/main/java/sandbox/semo/application/common/config/SecurityConfig.java
+++ semo-api/src/main/java/sandbox/semo/application/common/config/SecurityConfig.java
@@ -3,16 +3,15 @@ package sandbox.semo.application.common.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +22,7 @@ import sandbox.semo.application.security.authentication.MemberAuthProvider;
 @Log4j2
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
