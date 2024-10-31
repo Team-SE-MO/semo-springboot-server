87baeda70914dd9b27e36935027ac2409f5658bb
leejunehee <junehee107@naver.com>
2024-10-31 14:33:56 +0900

    :sparkles: feat: Jwt 토큰 통해 SecurityContext설정을 위한 userDetails 구현체 구현

---
+++ semo-api/src/main/java/sandbox/semo/application/security/authentication/JwtMemberDetails.java
@@ -0,0 +1,34 @@
+package sandbox.semo.application.security.authentication;
+
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class JwtMemberDetails implements UserDetails {
+
+    private final Long id;
+    private final Long companyId;
+    private final String role;
+    private final String loginId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return loginId;
+    }
+}
