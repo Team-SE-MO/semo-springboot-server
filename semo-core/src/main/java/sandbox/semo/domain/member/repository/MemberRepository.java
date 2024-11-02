45281cafd58b3e553ed68486f054381435658d77
heonuk <gitshineit@gmail.com>
2024-11-03 05:42:58 +0900

    :sparkles: feat: 통계 요약 정보 조회 비즈니스 로직 구현

--- semo-core/src/main/java/sandbox/semo/domain/member/repository/MemberRepository.java
+++ semo-core/src/main/java/sandbox/semo/domain/member/repository/MemberRepository.java
@@ -6,11 +6,12 @@ import sandbox.semo.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByIdAndDeletedAtIsNull(Long id);
+
     Optional<Member> findByLoginId(String loginId);
 
     Optional<Member> findByLoginIdAndDeletedAtIsNull(String loginId);
 
     Boolean existsByEmail(String email);
 
-    Optional<Member> findByEmailAndDeletedAtIsNull(String email);
 }
