f5abd61cb55a3d8fbe97f91072ad48c8f9dcbc64
heonuk <gitshineit@gmail.com>
2024-11-04 09:30:43 +0900

    :sparkles: feat: 장비별 차트 정보 조회 API Controller Layer 구현

--- semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
+++ semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
@@ -7,11 +7,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sandbox.semo.application.common.response.ApiResponse;
 import sandbox.semo.application.monitoring.service.MonitoringService;
 import sandbox.semo.application.security.authentication.JwtMemberDetails;
+import sandbox.semo.domain.monitoring.dto.request.DeviceMonitoring;
+import sandbox.semo.domain.monitoring.dto.response.DetailPageData;
 import sandbox.semo.domain.monitoring.dto.response.SummaryPageData;
 
 @Log4j2
@@ -28,6 +31,17 @@ public class MonitoringController {
             @AuthenticationPrincipal JwtMemberDetails memberDetails) {
         SummaryPageData data = monitoringService.fetchSummaryData(memberDetails.getId());
         return ApiResponse.successResponse(OK, "성공적으로 장비 요약 정보를 조회 하였습니다.", data);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @GetMapping("/chart")
+    public ApiResponse<DetailPageData> fetchDetailInfo(
+            @RequestBody DeviceMonitoring request,
+            @AuthenticationPrincipal JwtMemberDetails memberDetails
+    ) {
+        Long companyId = memberDetails.getCompanyId();
+        DetailPageData data = monitoringService.fetchDetailData(request, companyId);
+        return ApiResponse.successResponse(OK, "성공적으로 장비 차트 정보를 조회 하였습니다.", data);
     }
 
 }
