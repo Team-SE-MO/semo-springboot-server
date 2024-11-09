eb37bdbd5d5c14c2836b5bd39cbb3be4d0d1a3e3
heonuk <gitshineit@gmail.com>
2024-11-09 16:46:09 +0900

    :hammer: fix: 차트 데이터 조회 API 요청 방식 수정

--- semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
+++ semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class MonitoringController {
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
-    @GetMapping("/chart")
+    @PostMapping("/chart")
     public ApiResponse<DetailPageData> fetchDetailInfo(
             @RequestBody DeviceMonitoring request,
             @AuthenticationPrincipal JwtMemberDetails memberDetails
