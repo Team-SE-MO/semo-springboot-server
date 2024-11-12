263b325aaa7a7ff8af0b6648ccf87f2eab9ec880
heonuk <gitshineit@gmail.com>
2024-11-12 23:41:11 +0900

    :recycle: refactor: Email 발송 설정 파일 생성

---
+++ semo-api/src/main/java/sandbox/semo/application/common/config/EmailConfig.java
@@ -0,0 +1,36 @@
+package sandbox.semo.application.common.config;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public Session mailSession() {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        return Session.getInstance(props, new Authenticator() {
+           @Override
+           protected PasswordAuthentication getPasswordAuthentication() {
+               return new PasswordAuthentication(from, password);
+           }
+        });
+    }
+
+}
