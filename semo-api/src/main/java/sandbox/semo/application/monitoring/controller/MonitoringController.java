c3b24ba1615dbbed096879b8d8e3955e9687bbc2
dabeen-jung <ltea99@naver.com>
2024-11-14 07:11:22 +0900

    :sparkles: feat : 실시간 job 실행시간 조회 API - Controller layer 구현

--- semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
+++ semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
@@ -1,20 +1,27 @@
 package sandbox.semo.application.monitoring.controller;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.OK;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sandbox.semo.application.common.response.ApiResponse;
 import sandbox.semo.application.monitoring.service.MonitoringService;
 import sandbox.semo.application.security.authentication.JwtMemberDetails;
 import sandbox.semo.domain.monitoring.dto.request.DeviceMonitoring;
+import sandbox.semo.domain.monitoring.dto.response.DailyJobExecutionInfo;
 import sandbox.semo.domain.monitoring.dto.response.DetailPageData;
+import sandbox.semo.domain.monitoring.dto.response.MetaExecutionData;
+import sandbox.semo.domain.monitoring.dto.response.StepInfo;
 import sandbox.semo.domain.monitoring.dto.response.SummaryPageData;
 
 @Log4j2
@@ -44,4 +51,9 @@ public class MonitoringController {
         return ApiResponse.successResponse(OK, "성공적으로 장비 차트 정보를 조회 하였습니다.", data);
     }
 
-}
+    @GetMapping("/batch-chart")
+    public ApiResponse<MetaExecutionData> getRealTimeJobExecutionTimes() {
+        MetaExecutionData data = monitoringService.getRealTimeJobExecutionTimes();
+        return ApiResponse.successResponse(HttpStatus.OK, "성공적으로 현재 세션 Job의 실행시간을 조회했습니다.", data);
+    }
+
