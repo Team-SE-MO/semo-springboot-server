06c9b4a8d4d9bb8fb8beb5e9c23117d85d9e385d
dabeen-jung <ltea99@naver.com>
2024-11-17 01:12:24 +0900

    ✨ feat: 응답 dto 생성

---
+++ semo-core/src/main/java/sandbox/semo/domain/monitoring/dto/response/StepData.java
@@ -0,0 +1,31 @@
+package sandbox.semo.domain.monitoring.dto.response;
+
+
+import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Getter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StepData {
+
+    private int totalCount;
+    private Map<String, Integer> errorTypes;
+    private boolean hasError;
+
+    @Builder
+    public StepData(int totalCount, Map<String, Integer> errorTypes, boolean hasError) {
+        this.totalCount = totalCount;
+        this.errorTypes = errorTypes;
+        this.hasError = hasError;
+    }
+
+    @JsonProperty("hasError")
+    public boolean hasError() {
+        return hasError;
+    }
+}
---
+++ semo-core/src/main/java/sandbox/semo/domain/monitoring/dto/response/StepInfo.java
@@ -0,0 +1,12 @@
+package sandbox.semo.domain.monitoring.dto.response;
+
+import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class StepInfo {
+
+    private Map<String, StepData> stepExecution;
+}
