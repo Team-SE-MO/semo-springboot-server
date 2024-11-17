29bc769c6efa89606124eac70c4ef9f4f71b0ebe
dabeen-jung <ltea99@naver.com>
2024-11-17 16:28:24 +0900

    :hammer: fix : 중복 코드 삭제

--- semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
+++ semo-api/src/main/java/sandbox/semo/application/monitoring/controller/MonitoringController.java
@@ -2,18 +2,14 @@ package sandbox.semo.application.monitoring.controller;
 
 import static org.springframework.http.HttpStatus.OK;
 
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sandbox.semo.application.common.response.ApiResponse;
 import sandbox.semo.application.monitoring.service.MonitoringService;
@@ -55,19 +51,19 @@ public class MonitoringController {
     @GetMapping("/batch-chart")
     public ApiResponse<MetaExecutionData> getRealTimeJobExecutionTimes() {
         MetaExecutionData data = monitoringService.getRealTimeJobExecutionTimes();
-        return ApiResponse.successResponse(HttpStatus.OK, "성공적으로 현재 세션 Job의 실행시간을 조회했습니다.", data);
+        return ApiResponse.successResponse(OK, "성공적으로 현재 세션 Job의 실행시간을 조회했습니다.", data);
     }
 
     @GetMapping("/batch-chart/daily")
     public ApiResponse<DailyJobExecutionInfo> getDailyJobExecutionTimes() {
         DailyJobExecutionInfo data = monitoringService.getDailyJobExecutionTimes();
-        return ApiResponse.successResponse(HttpStatus.OK, "성공적으로 일별 Job의 실행시간을 조회했습니다.", data);
+        return ApiResponse.successResponse(OK, "성공적으로 일별 Job의 실행시간을 조회했습니다.", data);
     }
 
     @GetMapping("/failure")
     public ApiResponse<StepInfo> getStepErrorStatistics() {
         StepInfo data = monitoringService.getStepExecutionData();
-        return ApiResponse.successResponse(HttpStatus.OK, "성공적으로 배치 Step의 에러 통계를 조회했습니다.", data);
+        return ApiResponse.successResponse(OK, "성공적으로 배치 Step의 에러 통계를 조회했습니다.", data);
     }
 }
 
