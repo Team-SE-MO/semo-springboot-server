d1432a4875662aab9e7b1eb4b1cb2b3b0925b627
heonuk <gitshineit@gmail.com>
2024-11-20 02:19:51 +0900

    :bug: fix: 비활성 장비 최근 수집 기간 쿼리 수정

--- semo-core/src/main/java/sandbox/semo/domain/device/repository/DeviceRepository.java
+++ semo-core/src/main/java/sandbox/semo/domain/device/repository/DeviceRepository.java
@@ -12,7 +12,8 @@ public interface DeviceRepository extends JpaRepository<Device, Long> {
     @Query("SELECT d " +
             "FROM Device d " +
             "WHERE d.deviceAlias = :deviceAlias " +
-            "AND d.company.id = :companyId")
+            "AND d.company.id = :companyId " +
+            "AND d.deletedAt IS NULL")
     Optional<Device> findByAliasAndCompanyId(
             @Param("deviceAlias") String deviceAlias,
             @Param("companyId") Long companyId
@@ -21,7 +22,8 @@ public interface DeviceRepository extends JpaRepository<Device, Long> {
     @Query("SELECT d.id " +
             "FROM Device d " +
             "WHERE d.deviceAlias = :deviceAlias " +
-            "AND d.company.id = :companyId")
+            "AND d.company.id = :companyId " +
+            "AND d.deletedAt IS NULL")
     Long findIdByAliasAndCompanyId(
             @Param("deviceAlias") String deviceAlias,
             @Param("companyId") Long companyId
@@ -67,7 +69,8 @@ public interface DeviceRepository extends JpaRepository<Device, Long> {
                 END AS status, \
                 CASE \
                     WHEN COALESCE(m.BLOCKING_SESSION_COUNT, 0) >= 3 THEN m.BLOCKING_SESSION_COUNT \
-                    WHEN d.STATUS = 0 THEN EXTRACT(MINUTE FROM (SYSDATE - MAX(m.COLLECTED_AT))) \
+                    WHEN d.STATUS = 0 THEN
+                        FLOOR((CAST(CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Seoul' AS DATE) - CAST(MAX(m.COLLECTED_AT) AS DATE)) * 24 * 60)
                     ELSE COUNT(s.DEVICE_ID) \
                 END AS statusValue, \
                 MAX(m.COLLECTED_AT) AS lastCollectedAt \
@@ -81,6 +84,7 @@ public interface DeviceRepository extends JpaRepository<Device, Long> {
                                                       FROM SESSION_DATA s2
                                                       GROUP BY s2.DEVICE_ID)
             WHERE d.COMPANY_ID = :companyId
+            AND d.DELETED_AT IS NULL
             GROUP BY d.DEVICE_ALIAS, d.TYPE, d.IP, d.PORT, d.SID, d.STATUS, m.BLOCKING_SESSION_COUNT
             ORDER BY status DESC, statusValue DESC
             """, nativeQuery = true)
